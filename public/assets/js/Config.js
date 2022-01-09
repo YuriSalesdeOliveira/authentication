4309fb5e1a05cc465770eba2730a3a0c3cda98aa
YuriSalesdeOliveira <yuri_oli@hotmail.com>
2022-01-09 04:09:01 -0300

    Testing communication with api

--- public/assets/js/Config.js
+++ public/assets/js/Config.js
@@ -1,6 +1,6 @@
 export const Config = {
     "api": {
-        "root": "http://ec2-18-212-19-217.compute-1.amazonaws.com/api"
+        "root": "http://ec2-100-26-101-39.compute-1.amazonaws.com/api"
     },
     "site": {
         "root": "http://localhost/authentication/front/public"
